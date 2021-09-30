b2b3e6ebbad0f1085a54bc2ec6647a5c77eb39ab
Razi Rais <razibinrais@outlook.com>
2021-09-30 12:49:39 -0400

    Update install.sh

--- check-domain/install.sh
+++ check-domain/install.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-curl -o "check_b2cdomain.py" "https://raw.githubusercontent.com/razi-rais/Scarps/master/check-domain/check_b2cdomain.py" && curl -o "requirements.txt" "https://raw.githubusercontent.com/razi-rais/Scarps/master/check-domain/requirements.txt" && pip install -r  requirements.txt
+curl  -H "Cache-Control: no-cache"  -o "check_b2cdomain.py" "https://raw.githubusercontent.com/razi-rais/Scarps/master/check-domain/check_b2cdomain.py" && curl  -H "Cache-Control: no-cache"  -o "requirements.txt" "https://raw.githubusercontent.com/razi-rais/Scarps/master/check-domain/requirements.txt" && pip install -r  requirements.txt
