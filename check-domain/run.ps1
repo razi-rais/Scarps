2b4a1fa3cb8f693b5690d61d00df005ec1ed94a0
Razi Rais <razibinrais@outlook.com>
2021-09-30 11:40:24 -0400

    Update and rename run.ps1 to install.ps1

--- check-domain/run.ps1
+++
@@ -1,1 +0,0 @@
-curl -o "check_b2cdomain.py" "https://raw.githubusercontent.com/razi-rais/Scarps/master/check-domain/check_b2cdomain.py" && curl -o "requirements.txt" "https://raw.githubusercontent.com/razi-rais/Scarps/master/check-domain/requirements.txt" && pip install -r  requirements.txt && python check_b2cdomain.py -custom-domain "accountuat.contosobank.co.uk" -policy "b2c_1_susi" 
